75aed31d62b93caa296a170802270a97830a28bf
Roshan Shambharkar <roshanshambharkar@rediffmail.com>
2023-04-02 19:21:04 +0530

    get information form user with validation

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 require("dotenv").config()
 const express = require('express')
+const User = require('./model/User')
 
 const app = express();
-
+app.use(express.json())
 app.get("/", (req, res)=> {
-    res.send("<h1>Hello Form auth system - LCO</h1>")
+    res.send("<h1>Hello Form auth system</h1>")
+})
+
+app.post("/register", (req, res) => {
+    const {firstname, lastname, email, password} =  req.body;
+    if (!(email && password && firstname && lastname)) {
+        res.status(400).send('All fields are required')
+    }
+
+    const existingUser = User.findOne({email});
+
+    if(existingUser) {
+        res.status(401).send('User Already exists')
+    }
+
 })
 
 module.exports = app;
