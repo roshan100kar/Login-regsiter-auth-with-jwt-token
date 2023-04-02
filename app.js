dcf37bef478b6adb5e931810408237d08bf86c4e
Roshan Shambharkar <roshanshambharkar@rediffmail.com>
2023-04-02 20:38:36 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,25 +1,35 @@
 require("dotenv").config()
+require('./config/database').connect()
 const express = require('express')
-const User = require('./model/User')
+const bcrypt = require('bcryptjs')
 
+
+const User = require('./model/User')
 const app = express();
 app.use(express.json())
 app.get("/", (req, res)=> {
     res.send("<h1>Hello Form auth system</h1>")
 })
 
-app.post("/register", (req, res) => {
+app.post("/register", async(req, res) => {
     const {firstname, lastname, email, password} =  req.body;
     if (!(email && password && firstname && lastname)) {
         res.status(400).send('All fields are required')
     }
 
-    const existingUser = User.findOne({email});
+    const existingUser = await User.findOne({email}); // PROMISE
 
     if(existingUser) {
         res.status(401).send('User Already exists')
     }
 
+    const myEncPassword = await bcrypt.hash(password, 10) 
+    const user = await User.create({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        password: myEncPassword
+    })
 })
 
 module.exports = app;
