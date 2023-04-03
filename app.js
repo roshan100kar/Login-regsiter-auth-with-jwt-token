ad7dd790bdbca014cda6900dabe87fe0ce80c59e
Roshan Shambharkar <roshanshambharkar@rediffmail.com>
2023-04-03 14:09:57 +0530

    dash board raoutes added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 require('./config/database').connect()
 const express = require('express')
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 
 const User = require('./model/User')
 const app = express();
@@ -12,24 +12,84 @@ app.get("/", (req, res)=> {
 })
 
 app.post("/register", async(req, res) => {
-    const {firstname, lastname, email, password} =  req.body;
-    if (!(email && password && firstname && lastname)) {
-        res.status(400).send('All fields are required')
+    try {
+        const {firstname, lastname, email, password} =  req.body;
+        if (!(email && password && firstname && lastname)) {
+            res.status(400).send('All fields are required')
+        }
+    
+        const existingUser = await User.findOne({email}); // PROMISE
+    
+        if(existingUser) {
+            res.status(401).send('User Already exists')
+        }
+    
+        const myEncPassword = await bcrypt.hash(password, 10) 
+        const user = await User.create({
+            firstname,
+            lastname,
+            email: email.toLowerCase(),
+            password: myEncPassword
+        })
+    
+        //token
+        const token = jwt.sign(
+            {user_id: user._id, email},
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "2h"
+            }
+        )
+        user.token = token
+        //UPdate or not
+
+        //handle password situation
+        user.password = undefined;
+
+        //send token or send just success yes and redirect - choice
+        res.status(201).json(user)
+
+    } catch (error) {
+        console.log(error)
     }
+})
 
-    const existingUser = await User.findOne({email}); // PROMISE
+app.post("/login", async (req, res)=> {
+    try {
+      const {email, password} = req.body
+      if(!(email && password)) {
+        res.status(400).send("Field is missing")
+      }  
 
-    if(existingUser) {
-        res.status(401).send('User Already exists')
+     const user = await User.findOne({email})
+
+    //   if(!user) {
+    //     res.status(400).send("You are not registered in our app")
+    //   }
+
+      if(user && (await bcrypt.compare(password, user.password))) {
+        const token = jwt.sign(
+            {user_id: user._id, email },
+            process.env.SECRET_KEY,
+            {
+                expiresIn: "2h"
+            }
+        )
+        user.token = token
+        user.password = undefined
+        res.status(200).json(user)
+      }
+
+      res.send(400).send("email or password is incorrect")
+
+
+    } catch (error) {
+        console.log(error)
     }
+})
 
-    const myEncPassword = await bcrypt.hash(password, 10) 
-    const user = await User.create({
-        firstname,
-        lastname,
-        email: email.toLowerCase(),
-        password: myEncPassword
-    })
+app.get("/dashboard", (req, res)=> {
+    res.send("Welcome to secret information");
 })
 
 module.exports = app;
